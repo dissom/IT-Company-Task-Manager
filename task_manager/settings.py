3fa6464b8cf12dd8a3891b354c92a9b99611fcea
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-04-23 23:41:50 +0300

    Fixed bug in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = "RENDER" not in os.environ
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 # ALLOWED_HOSTS = []
 
